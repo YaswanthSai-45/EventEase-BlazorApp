2dee8b8933bf4cad03aef981197331dfc78139cc
YaswanthSai-45 <kummaray83@gmail.com>
2025-07-05 22:17:17 +0530

    Create EventModel.cs

---
+++ EventModel.cs
@@ -0,0 +1,7 @@
+public class EventModel
+{
+    public string Title { get; set; }
+    public string Location { get; set; }
+    public DateTime Date { get; set; }
+    public bool IsRegistered { get; set; } = false;
+}
